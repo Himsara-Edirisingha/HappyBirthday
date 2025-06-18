3e92e2eaea23edf88a1890c5132bf00631895d20
arulpragasam-se <lihini789@gmail.com>
2025-06-18 22:12:09 +0530

    config base

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-   preview: {
+  base: '/',
+  preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 4173,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
     allowedHosts: ['ios-back-end.onrender.com']
   }
-})
+});
